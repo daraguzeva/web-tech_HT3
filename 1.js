41e07a1e41985a0da0bcc9a293ea18b1fd116599
Daria <lara-lorn@yandex.ru>
2023-04-28 20:09:15 +0400

    save 1.js

---
+++ 1.js
@@ -0,0 +1,3 @@
+const tCelsius = +prompt('Введите темпратуру в градусах Цельсия');
+const tFarenheit = (9 / 5) * tCelsius + 32;
+// alert(`Цельсий: ${tCelsius}, Фаренгейт: ${tFarenheit.toFixed(1)}`);
